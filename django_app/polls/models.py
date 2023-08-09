9e80f9430f4e6f6c6979e382dd63574b730b2233
caamartinezbu <carlosmartinez622@gmail.com>
2023-08-09 11:48:46 -0500

    creacion de las vistas  en la app polls en el archivo views.py por medio de funsiones y redireccionadas en urls.py

--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -3,20 +3,22 @@ from django.utils import timezone
 import datetime
 
 
+
 # Create your models here.
 
 
 class Question (models.Model):
     question_text = models.CharField(max_length=200) 
     pub_date = models.DateTimeField("date published / dato de publicación")
-
+    #método para visualizar los registros por medio de la shell interactive
     def __str__(self):
         return self.question_text
-    
+        #método para saber si una pregunta es actual menor a un dia de publicada
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice (models.Model):
+    #question= models.ForeignKey(Question, on_delete=models.CASCADE, related_name=‘question_choices’)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    return HttpResponse("<h1>estas en la pagina principal de la aplicación</H1> ")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"estas viendo la pregunta numero {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"estas viendo los resultados de la pregunta numero {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"estas votando a la pregunta numero {question_id}")
+
